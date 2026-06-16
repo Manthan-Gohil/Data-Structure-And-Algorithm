3bfb21b2b942e2832c9d1a6ea42f8494775a9bd8
Manthan-Gohil <manthangohil22577@gmail.com>
2026-06-16 13:15:03 +0530

    implementing queue using linked list

---
+++ Stack_Queue/Queue/01_queue.cpp
@@ -0,0 +1,90 @@
+// Queue -> FIFO (front and rear end)
+// operations = 
+// 1. push (add from the rear end) ->enqueue
+// 2. pop  (remove from front end) - dequeue
+// 3. front operation 
+// each operation take TC(O(1))
+
+// implement queue using linked list
+// front -> head of the linked list head->data
+// push -> insert data at tail of linked list
+// pop -> delete the head node and head = head->next
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+class Queue{
+    Node* head;
+    Node* tail;
+    
+    public:
+    Queue(){
+        head = tail = NULL;
+    }
+
+    void push(int value){ // insert value at tail
+        if(empty()){
+            Node* temp = new Node(value);
+            head = tail = temp;
+        }
+        else{
+            Node* temp = new Node(value);
+            tail->next = temp;
+            tail = temp;
+        }
+    }
+
+    void pop(){ // head  = head->next;
+        if(empty()){
+            cout<<"Queue is already empty"<<endl;
+        }
+        else{
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+
+    }
+
+    int front(){
+        if(empty()){
+            return -1;
+        }
+        else{
+            return head->data;
+        }
+
+    }
+
+    bool empty(){
+        return head == NULL;
+    }
+
+
+};
+
+int main(){
+    Queue q;
+
+    q.push(1);
+    q.push(2);
+    q.push(3); // 1, 2, 3
+
+    while(!q.empty()){
+        cout<<q.front()<<" ";
+        q.pop();
+    }
+    cout<<endl;
+    return 0;
+
+}
