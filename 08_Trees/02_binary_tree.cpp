de79ab895898742bf0e0e80a5b1d059332d508c0
Manthan-Gohil <manthangohil22577@gmail.com>
2026-06-18 11:53:41 +0530

    binary trees traversals

--- 08_Trees/02_binary_tree.cpp
+++ 08_Trees/02_binary_tree.cpp
@@ -14,7 +14,7 @@ class Node{
     }
 };
 
-// preorder build tree
+// preorder build tree: TC O(n)
 static int idx = -1;
 Node* buildTree(vector<int> &preorder){
     idx++;
@@ -27,13 +27,106 @@ Node* buildTree(vector<int> &preorder){
     return root;
 }
 
+// preorder traversal TC(O(N))
+void preOrder(Node* root){
+    if(root == NULL) return;
+
+    cout<<root->data<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+// inorder traversal TC(O(N))
+void inOrder(Node* root){
+    if(root == NULL) return;
+
+    inOrder(root->left);
+    cout<<root->data<<" ";
+    inOrder(root->right);
+}
+
+// postorder traversal TC(O(N))
+void postOrder(Node* root){
+    if(root == NULL) return;
+
+    postOrder(root->left);
+    postOrder(root->right);
+    cout<<root->data<<" ";
+}
+
+// level order traversal (iterative way) using queue
+void levelOrder(Node* root){
+    queue<Node*> q;
+    if(root == NULL) return;
+    q.push(root);
+
+    while(q.size() > 0){
+        Node* curr = q.front();
+        q.pop();
+        cout<<curr->data<<" ";
+
+        if(curr->left != NULL){
+            q.push(curr->left);
+        }
+        if(curr->right != NULL){
+            q.push(curr->right);
+        }
+    }
+    cout<<endl;
+}
+
+// level order traversal (with each level have new line)
+void levelOrder2(Node* root){
+    queue<Node*> q;
+    if(root == NULL) return;
+
+    q.push(root);
+    q.push(NULL);
+
+    while(q.size() > 0){
+        Node* curr = q.front();
+        q.pop();
+
+        if(curr == NULL){
+            if(!q.empty()){
+                cout<<endl;
+                q.push(NULL);
+                continue;
+            }else{
+                break;
+            }
+        }
+
+        cout<<curr->data<<" ";
+
+        if(curr->left != NULL){
+            q.push(curr->left);
+        }
+        if(curr->right != NULL){
+            q.push(curr->right);
+        }
+    }
+    cout<<endl;
+}
+
 int main(){
     vector<int> preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1};
 
     Node* root = buildTree(preorder);
     cout<<root->data<<endl;
-    cout<<root->left->data<<endl;
-    cout<<root->right->data<<endl;
-
+    
+    cout<<"preorder traversal : ";
+    preOrder(root);
+    cout<<endl;
+    cout<<"inorder traversal : ";
+    inOrder(root);
+    cout<<endl;
+    cout<<"postorder traversal : ";
+    postOrder(root);
+    cout<<endl;
+    cout<<"levelorder traversal : ";
+    levelOrder(root);
+    cout<<"levelorder traversal 2 : "<<endl;
+    levelOrder2(root);
     return 0;
 }
