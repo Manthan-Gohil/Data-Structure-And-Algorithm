22f4e3da4b30d7475186e31ddfed9b10170e4a4f
Manthan-Gohil <manthangohil22577@gmail.com>
2026-06-18 10:39:52 +0530

    basics of binary tree

---
+++ 08_Trees/01_binary_tree.cpp
@@ -0,0 +1,24 @@
+// Trees is a hierarchial type data structure and not linear 
+// as arrays, vectors, linked list all are linear data structure
+// Trees store data in the nodes
+
+// Generic Tree ->
+// main node in tree is root node
+// multiples nodes can be connected with same node
+// branches connect two different nodes
+// parent and child node
+// next level (below) nodes are child node of above node connected to it
+// parent node are the node which connect nodes below it.
+
+// Binary Tree
+// Tree having each node is having at max 2 child node
+// last nodes at base are leaf nodes, leaf nodes do not have child nodes
+// top node are root node
+// sibling nodes are those nodes which are at same level and have same parent
+// height/depth of tree  = total levels of tree (if level start with 1)
+// subtree -> small part of a tree (eg, left subtree and right subtree)
+
+// preorder = root->left->right
+// inorder = left->root->right
+// postorder = left->right->root
+
