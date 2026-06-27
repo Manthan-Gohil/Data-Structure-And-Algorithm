5935240d0b48d283dabb100e1280a090b60da113
Manthan-Gohil <manthangohil22577@gmail.com>
2026-06-27 21:42:08 +0530

    Problem : Kth largest smallest BST

---
+++ 09_Binary_Search_Tree/Questions/05_kthLargest.cpp
@@ -0,0 +1,110 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d){
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node* insertToTree(Node* root, int d){
+    // base case
+    if(root == NULL){
+        root = new Node(d);
+        return root;
+    }
+
+    if(d < root->data){
+        // left part me insert krna h
+        root->left = insertToTree(root->left,d);
+    }
+    else{
+        // right part me insert krna hai
+        root->right = insertToTree(root->right,d);
+    }
+    return root;
+}
+
+void take_input(Node* &root){
+    int data;
+    cin>>data;
+
+    while(data != -1){
+        root = insertToTree(root, data);
+        cin>>data;
+    }
+}
+
+void levelOrderTraversal(Node* root){
+    queue<Node*> q;
+    if(root == NULL) return;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        if(temp == NULL){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<<temp->data<<" ";
+            if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+        }
+    }
+}
+// i is pass by reference means every recursive call shares the same i.
+int solve(Node* root, int& i, int k){
+    if(root == NULL){
+        return -1;
+    }
+    // reverse of inorder traversal
+    // Right
+    int right = solve(root->right, i, k);
+    if(right != -1){
+        return right;
+    }
+    // Node
+    i++;
+    if(i==k){
+        return root->data;
+    }
+
+    // Right
+    return solve(root->left,i,k);
+
+}
+
+int kthLargest(Node* root, int k){
+    int i=0;
+    int ans = solve(root,i,k);
+    return ans;
+}
+
+int main(){
+    Node* root = NULL;
+    cout<<"Build BST : "<<endl;
+    // 5,4,8,2,6,10,3,7
+    take_input(root);
+
+    cout<<"level order traversal : "<<endl;
+    levelOrderTraversal(root);
+
+    cout<<"Kth Largest : "<<kthLargest(root,3);
+
+}
