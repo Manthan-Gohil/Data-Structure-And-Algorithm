22410d365948dd43372bafde694d337e6c386edd
Manthan-Gohil <manthangohil22577@gmail.com>
2026-07-01 14:40:45 +0530

    Problem : is BT a heap

---
+++ 10_Heaps/Questions/04_is_BT_heap.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+int countNodes(Node* root){
+    // base case
+    if(root == NULL) return 0;
+
+    int ans = 1 + countNodes(root->left) + countNodes(root->right);
+    return ans;
+}
+
+bool isCBT(Node* root, int index, int cnt){
+    if(root == NULL) return true;
+
+    if(index >= cnt){
+        return false;
+    }
+    else{
+        bool left = isCBT(root->left, 2*index+1, cnt);
+        bool right = isCBT(root->right, 2*index+2,cnt);
+        return (left && right);
+    }
+}
+
+bool isMaxOrder(Node* root){
+    // leaf node
+    if(root->left == NULL && root->right == NULL){
+        return true;
+    }
+    if(root->right == NULL){
+        return (root->data > root->left->data);
+    }else{
+        bool left = isMaxOrder(root->left);
+        bool right = isMaxOrder(root->right);
+
+        if(left && right && (root->data > root->left->data && root->data > root->left->data)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+bool isHeap(Node* root){
+    int index = 0;
+    int totalCount = countNodes(root);
+    if(isCBT(root, index, totalCount) && isMaxOrder(root)){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+int main(){
+
+}
