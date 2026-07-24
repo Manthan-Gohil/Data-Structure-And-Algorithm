86e48d4ebecd4cde0e867417409e5d631d40dfa9
Manthan-Gohil <manthangohil22577@gmail.com>
2026-07-24 10:24:56 +0530

    Problem : wiggle sort 2 problem

--- 02_Array-Question/15_wiggle_sort_2.cpp
+++ 02_Array-Question/15_wiggle_sort_2.cpp
@@ -1,7 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void wiggleSort(vector<int> &nums){
+    int n = nums.size();
+    vector<int> temp = nums;
+    sort(temp.begin(), temp.end());
+
+    int left = (n-1)/2;
+    int right = n-1;
+    
+    for(int i=0;i<n;i++){
+        if(i%2 == 0){
+            nums[i] = temp[left];
+            left--;
+        }else{
+            nums[i] = temp[right];
+            right--;
+        }
+    }
+    
+}
 
 int main(){
+    vector<int> nums = {1,5,1,1,6,4};
 
+    wiggleSort(nums);
+
+    for(int num : nums){
+        cout<<num<<" ";
+    }
 }
---
+++ Daily_Leetcode_Practice/10_unique_xor_triplets_2.cpp
@@ -0,0 +1,11 @@
+// leetcode no - 3514
+#include<bits/stdc++.h>
+using namespace std;
+
+int uniqueXorTriplets(vector<int> &nums){
+    
+}
+
+int main(){
+
+}
