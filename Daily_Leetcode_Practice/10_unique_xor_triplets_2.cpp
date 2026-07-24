7213702e000bb202f83881257f343ce3f61f91c6
Manthan-Gohil <manthangohil22577@gmail.com>
2026-07-24 10:30:30 +0530

    Problem : unique xor triplet 2

--- Daily_Leetcode_Practice/10_unique_xor_triplets_2.cpp
+++ Daily_Leetcode_Practice/10_unique_xor_triplets_2.cpp
@@ -3,9 +3,29 @@
 using namespace std;
 
 int uniqueXorTriplets(vector<int> &nums){
-    
+    int n = nums.size();
+
+    unordered_set<int> pairXor;
+
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            pairXor.insert(nums[i]^nums[j]);
+        }
+    }
+
+    unordered_set<int> tripletXor;
+
+    for(int x : pairXor){
+        for(int num : nums){
+            tripletXor.insert(x ^ num);
+        }
+    }
+    return tripletXor.size();
 }
 
 int main(){
+
+    vector<int> nums = {6,7,8,9};
+    cout<<"unique xor triptlet = "<<uniqueXorTriplets(nums)<<endl;
 
 }
